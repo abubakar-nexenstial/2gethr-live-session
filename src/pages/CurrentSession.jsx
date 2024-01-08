99e91d625eedb221a858ba9da4053800c88e639a
abubakar-nexenstial <abubakar@nexenstial.com>
2024-01-08 14:40:24 +0530

    License Added

--- src/pages/CurrentSession.jsx
+++ src/pages/CurrentSession.jsx
@@ -9,7 +9,7 @@ export default function CurrentSession() {
     <section className="bg-cover bg-center min-h-screen w-full pt-10" >
       <div className='w-full flex justify-center align-items-center text-white '>
 
-    <span className='backdrop-blur-md bg-white/15 border-[#424242] border-2 rounded-md px-10 py-3 shadow-2xl'>Timing</span>
+    <span className='backdrop-blur-md bg-white/15 border-[#424242] border-2 rounded-md px-12 py-3 shadow-2xl'>Device License : NLV8HDOCKKREEV6T</span>
       </div>
       
       <div className='flex justify-center align-items-center w-full pt-2'>
